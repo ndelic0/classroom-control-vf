efe11a0e3723be1d2a1fd89c23cf1713ad01eff6
Nemanja Delic <ndelic0@users.noreply.github.com>
2016-06-08 10:53:59 +0200

    fix spaces

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,13 +46,11 @@ node default {
 
   notify { "Hello, my name is ${::hostname}": }
 
- file { '/etc/motd':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  content => "Today I learned what it means to manage state using Puppet.\n",
- }
-
-
+  file { '/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => "Today I learned what it means to manage state using Puppet.\n",
+  }
 }
