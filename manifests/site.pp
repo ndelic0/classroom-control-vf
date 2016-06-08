934554bf7c186ed02a433c4fe43aece4f618e493
Nemanja Delic <ndelic0@users.noreply.github.com>
2016-06-08 10:40:55 +0200

    test file 2

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,13 +46,13 @@ node default {
 
   notify { "Hello, my name is ${::hostname}": }
 
-  file {'/etc/motd':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    content => "Think before you type\n",
-  }
+ file { '/etc/motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  content => "Today I learned what it means to manage state using Puppet.\n",
+ }
 }
 
 }
