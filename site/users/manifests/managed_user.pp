c6ef65083e1cfbcd7f3f3f56324d7eb716ca8089
Nemanja Delic <ndelic0@users.noreply.github.com>
2016-06-09 14:37:31 +0200

    manage_user define

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,17 @@
+define users::managed_user (
+ group => $title,
+)
+{
+
+  user { $title:
+    ensure => present,
+    group  => $group,
+  }
+  
+  file { "/home/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $title,
+  }
+
+}
