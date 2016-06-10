751a0f81df6457e81b576c5d6eadf3215bd71b82
Nemanja Delic <n.delic@levi9.com>
2016-06-10 09:49:16 +0200

    Excercise 18.2

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,32 +1,13 @@
 #
-class nginx {
-  case $::osfamily {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-    }
-    'windows' : {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
-    }
-    default : {
-      fail("Module ${module_name} is not supported on ${::osfamily}")
-    }
-  }
-
-  $user = $::osfamily ? {
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
-    'windows' => 'nobody',
-  }
+class nginx (
+  $package = $nginx::params::package,
+  $owner = $nginx::params::owner,
+  $group = $nginx::params::group,
+  $docroot = $nginx::params::docroot,
+  $confdir = $nginx::params::confdir,
+  $logdir = $nginx::params::logdir,
+  $user = $nginx::params::user,
+) inherits nginx::params {
 
   File {
     owner => $owner,
