e08833900d486c1faea7d9bbfe50c2c92b8c907f
Nemanja Delic <n.delic@levi9.com>
2016-06-10 13:24:18 +0200

    adding mysqwl server on wordpress

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,7 +1,20 @@
 class profile::wordpress {
+
+  group { 'wordpress':
+    ensure => present,
+  }
+
+  user { 'wordpress':
+    ensure => present,
+    git    => 'wordpress',
+  }
+
   include apache
+  include apache::mod::php
+
+  include mysql::server
+  include mysql::server::bindings
+
+
   include wordpress
-  include mysql::client
-
 }
-
